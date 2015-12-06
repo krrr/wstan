3b8dbd46fc8fbe78ceefc1983905aece7896ae22
krrr <guogaishiwo@gmail.com>
2015-12-06 20:41:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import wstan
 
 
 setup(name='wstan',
       author=wstan.__author__,
       version=wstan.__version__,
-      description='',
+      description='Tunneling TCP connection in WebSocket',
       url='https://krrr.github.io',
-      packages=['wstan'],
+      packages=find_packages(),
       requires=['cryptography'],
       entry_points={'console_scripts': ['wstan = wstan:main_entry']})
